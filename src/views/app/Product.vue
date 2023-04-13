203fbbf1a4223e22214dffcb201a3e3064b30df6
rukkiecodes <rukkiecodes@gmail.com>
2023-04-14 00:14:34 +0100

    done displaying product info

--- src/views/app/Product.vue
+++ src/views/app/Product.vue
@@ -1,34 +1,91 @@
 <template>
     <v-container>
-        <v-row>
-            <v-col cols="12" sm="5">
+        <v-card rounded="xl">
+            <v-card-text>
+                <v-row>
+                    <v-col cols="12" sm="5">
+                        <v-card flat>
+                            <v-img :src="product?.image" />
+                        </v-card>
+                    </v-col>
+                    <v-col cols="12" sm="7">
+                        <v-card flat>
+                            <v-card-text class="text-h5 font-weight-bold" style="line-height: 1.5em;">{{ product?.name
+                            }}</v-card-text>
+                            <v-card-text class="d-flex justify-space-between"><span class="text-h5 font-weight-bold">₦ {{
+                                product?.price?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") }}</span> <span
+                                    class="font-weight-bold">{{
+                                        product?.availability }}</span></v-card-text>
+                            <v-card-text class="d-flex justify-space-between"><span class="font-weight-bold">+Shipping ₦{{
+                                product?.shippingCost.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+                            }}</span></v-card-text>
+
+                            <v-card-text>
+                                <span class="text-grey-darken-3">Items left: {{
+                                    product?.quantity.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") }}</span>
+                                <v-progress-linear :model-value="product?.quantity" color="blue" class="mt-2" />
+                            </v-card-text>
+
+                            <v-card-title class="text-body-1">VARIATION AVAILABLE</v-card-title>
+                            <v-card-text>
+                                <v-btn v-for="(variation, i) in product?.variations" :key="i" size="small"
+                                    class="ma-2 text-caption" variant="outlined" color="blue" rounded="lg">{{ variation
+                                    }}</v-btn>
+                            </v-card-text>
+
+                            <v-card-actions>
+                                <v-btn size="x-large" class="bg-blue" rounded="xl" style="flex: 1;">Add to cart</v-btn>
+                                <v-btn icon size="large">
+                                    <v-icon>mdi-heart-outline</v-icon>
+                                </v-btn>
+                            </v-card-actions>
+                        </v-card>
+                    </v-col>
+                </v-row>
+            </v-card-text>
+        </v-card>
+
+        <v-row class="mt-4">
+            <v-col cols="12" sm="8" order="1" order-sm="0">
                 <v-card rounded="xl">
-                    <v-img :src="product?.image" />
+                    <v-card-title>Product details</v-card-title>
+                    <v-card-text>{{ product?.description }}</v-card-text>
+
+                    <v-card-title>Brand</v-card-title>
+                    <v-card-text>{{ product?.brand }}</v-card-text>
+
+                    <v-card-title>Product Condition</v-card-title>
+                    <v-card-text>{{ product?.condition == '' ? 'N/A' : product?.condition }}</v-card-text>
+
+                    <v-card-title>Product weight</v-card-title>
+                    <v-card-text>{{ product?.weight == '' ? 'N/A' : product?.weight }}KG</v-card-text>
+
+                    <v-card-title>Shipping Options</v-card-title>
+                    <v-card-text>
+                        <v-chip v-for="(option, i) in product?.shippingOptions" :key="i" class="text-capitalize ma-2">{{
+                            option }}</v-chip>
+                    </v-card-text>
                 </v-card>
             </v-col>
-            <v-col cols="12" sm="7">
+            <v-col cols="12" sm="4">
                 <v-card rounded="xl">
-                    <v-card-text class="text-h5 font-weight-bold" style="line-height: 1.5em;">{{ product?.name
-                    }}</v-card-text>
-                    <v-card-text class="d-flex justify-space-between"><span class="text-h5 font-weight-bold">₦ {{
-                        product?.price?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") }}</span> <span
-                            class="font-weight-bold">{{
-                                product?.availability }}</span></v-card-text>
-                    <v-card-text class="d-flex justify-space-between"><span class="font-weight-bold">+Shipping ₦{{
-                        product?.shippingCost.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") }}</span></v-card-text>
+                    <v-card-title>Seller Information</v-card-title>
+                    <v-card-title class="text-body-1">{{ profile?.name }}</v-card-title>
 
-                    <v-card-title class="text-body-1">VARIATION AVAILABLE</v-card-title>
-                    <v-card-text>
-                        <v-btn v-for="(variation, i) in product?.variations" :key="i" size="large" class="mr-2"
-                            variant="outlined" color="blue" rounded="lg">{{ variation }}</v-btn>
-                    </v-card-text>
+                    <v-rating :model-value="profile?.rating == undefined ? 0 : profile?.rating" color="amber-darken-4" />
+                </v-card>
+            </v-col>
+        </v-row>
 
-                    <v-card-actions>
-                        <v-btn size="x-large" class="bg-blue" rounded="xl" style="flex: 1;">Add to cart</v-btn>
-                        <v-btn icon size="large">
-                            <v-icon>mdi-heart-outline</v-icon>
-                        </v-btn>
-                    </v-card-actions>
+        <v-row>
+            <v-col v-for="(good, i) in goods" :key="i" cols="12" sm="6" md="3">
+                <v-card rounded="xl" @click="openProduct(good.id)">
+                    <v-img :src="good.image" />
+
+                    <v-card-title>{{ good.name }}</v-card-title>
+                    <v-card-text class="d-flex justify-space-between"><span>₦ {{
+                        good.price?.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") }}</span> <span>{{
+        good.availability }}</span></v-card-text>
                 </v-card>
             </v-col>
         </v-row>
@@ -37,11 +94,14 @@
 
 <script>
 import { db } from '@/plugins/firebase';
-import { doc, getDoc } from '@firebase/firestore';
+import { collection, doc, getDoc, getDocs, limit, query, where } from '@firebase/firestore';
+import router from '@/router';
 
 export default {
     data: () => ({
-        product: null
+        product: null,
+        profile: null,
+        goods: []
     }),
     mounted() {
         this.$nextTick(() => {
@@ -56,6 +116,34 @@ export default {
                 id: prop,
                 ...product
             }
+
+            this.getSellerInfo(product.seller)
+            this.getSimilar(product.subcategory)
+        },
+
+        async getSellerInfo(prop) {
+            let user = (await getDoc(doc(db, 'users', prop))).data()
+
+            this.profile = user
+        },
+
+        async getSimilar(prop) {
+            const q = query(collection(db, "ads"), where("subcategory", "==", prop), limit(100))
+
+            const querySnapshot = await getDocs(q)
+
+            this.goods = []
+            querySnapshot.forEach((doc) => {
+                this.goods.push({
+                    id: doc.id,
+                    ...doc.data()
+                })
+            });
+        },
+
+        openProduct(prop) {
+            router.push(`/app/${prop}`)
+            this.getProduct(prop)
         }
     }
 }
